0f6eef65874fab8bb1179bc9b08d6b330a10883e
Carles Serna Uroz <carlesserna@MacBook-Pro-de-Carles.local>
2017-07-21 16:19:28 +0200

    Firsts steps - toolbar done

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <v-toolbar>
+      <v-toolbar-title>DevMeetup</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>event</v-icon>
+          View Meetups
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+      
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
