ce5cd38982d3f1b4af1a66262adea28f4a76d5be
Carles Serna Uroz <carlesserna@MacBook-Pro-de-Carles.local>
2017-08-23 15:38:37 +0200

    menu inside array

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
 <template>
   <v-app>
     <v-navigation-drawer v-model="sideNav">
-      
+      <v-list>
+        <v-list-tile v-for="item in menuItems" :key="item.title">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>{{ item.title }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
     </v-navigation-drawer>
-    <v-toolbar>
-      <v-toolbar-side-icon @click.native.stop="sideNav = !sideNav"></v-toolbar-side-icon><!-- burguer -->
+    <v-toolbar class="primary">
+      <v-toolbar-side-icon 
+        @click.native.stop="sideNav = !sideNav"
+        class="hidden-sm-and-up">
+      </v-toolbar-side-icon><!-- burguer -->
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn flat>
-          <v-icon left>event</v-icon>
-          View Meetups
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn flat><!-- v-for="item in menuItems" :key="item.title" -->
+          <v-icon left><!-- {{ items.icon }} --></v-icon>
+          <!-- {{ item.title }} -->
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <main>
-      
+    
     </main>
   </v-app>
 </template>
@@ -24,7 +34,14 @@
   export default {
     data () {
       return {
-        sideNav: false
+        sideNav: false,
+        menuItems: [
+          { icon: 'supervisor_account', title: 'View Meetups' },
+          { icon: 'room', title: 'Organize Meetup' },
+          { icon: 'person', title: 'Profile' },
+          { icon: 'face', title: 'Sign Up' },
+          { icon: 'lock_open', title: 'Sign in' }
+        ]
       }
     }
   }
